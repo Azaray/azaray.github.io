930109fc8b14062fa6f7becfec74356bc68eefbc
Leonardo <leonardoroelofse@gmail.com>
2018-06-13 22:52:38 +0100

    Disabled title animation for mobile

--- js/main.js
+++ js/main.js
@@ -1,56 +1,55 @@
+$(document).ready(function () {
+    "use strict";
 
-$(document).ready(function(){
-	"use strict";
-
-	var window_width 	 = $(window).width(),
-	window_height 		 = window.innerHeight,
-	header_height 		 = $(".default-header").height(),
-	header_height_static = $(".site-header.static").outerHeight(),
-	fitscreen 			 = window_height - header_height;
+    var window_width = $(window).width(),
+        window_height = window.innerHeight,
+        header_height = $(".default-header").height(),
+        header_height_static = $(".site-header.static").outerHeight(),
+        fitscreen = window_height - header_height;
 
 
-	$(".fullscreen").css("height", window_height)
-	$(".fitscreen").css("height", fitscreen);
+    $(".fullscreen").css("height", window_height)
+    $(".fitscreen").css("height", fitscreen);
 
-     
-     // -------   Active Mobile Menu-----//
 
-    $(".menu-bar").on('click', function(e){
+    // -------   Active Mobile Menu-----//
+
+    $(".menu-bar").on('click', function (e) {
         e.preventDefault();
         $("nav").toggleClass('hide');
         $("span", this).toggleClass("lnr-menu lnr-cross");
         $(".main-menu").addClass('mobile-menu');
     });
-     
+
     $('select').niceSelect();
     $('.img-pop-up').magnificPopup({
         type: 'image',
-        gallery:{
-        enabled:true
+        gallery: {
+            enabled: true
         }
     });
 
     $('.active-project-carousel').owlCarousel({
         center: true,
-        items:1,
-        loop:true,
+        items: 1,
+        loop: true,
         margin: 100,
         nav: true,
         navText: ['<i class="fa fa-caret-left""></i>', '<i class="fa fa-caret-right""></i>']
     });
     $('.active-banner-slider').owlCarousel({
-        items:1,
-        loop:true,
+        items: 1,
+        loop: true,
         margin: 100,
         dots: true
     });
     // Add smooth scrolling to Menu links
 
-    $(document).ready(function(){
+    $(document).ready(function () {
         var screenWidth = $(window).width();
         // if window width is smaller than 800 remove the autoplay attribute
         // from the video
-        if (screenWidth < 800){
+        if (screenWidth < 800) {
             $('video').removeAttr('autoplay');
 
         } else {
@@ -58,18 +57,18 @@ $(document).ready(function(){
         }
     });
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('#mc_embed_signup').find('form').ajaxChimp();
-    });      
+    });
     // -------   Mail Send ajax
 
-     $(document).ready(function() {
+    $(document).ready(function () {
         var form = $('#myForm'); // contact form
         var submit = $('.submit-btn'); // submit button
         var alert = $('.alert-msg'); // alert div for show alert message
 
         // form submit event
-        form.on('submit', function(e) {
+        form.on('submit', function (e) {
             e.preventDefault(); // prevent default form submit
 
             $.ajax({
@@ -77,19 +76,20 @@ $(document).ready(function(){
                 type: 'POST', // form submit method get/post
                 dataType: 'html', // request type html/json/xml
                 data: form.serialize(), // serialize form data
-                beforeSend: function() {
+                beforeSend: function () {
                     alert.fadeOut();
                     submit.html('Sending....'); // change submit button text
                 },
-                success: function(data) {
+                success: function (data) {
                     alert.html(data).fadeIn(); // fade in response data
                     form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
+                    submit.attr("style", "display: none !important");
+                    ; // reset submit button text
                 },
-                error: function(e) {
+                error: function (e) {
                     console.log(e)
                 }
             });
         });
     });
- });
+});
